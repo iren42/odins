bce30a4a26727a44042c81fcd9f17e5c9c4cb1c9
iren42 <isabelleren618@gmail.com>
2025-01-27 14:54:59 +0100

    feat: modify array

--- foundations-js/script.js
+++ foundations-js/script.js
@@ -1,54 +1,28 @@
-console.log("Hello");
+/*
+ * Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 
-const container = document.querySelector("#container");
+The function should only modify the array. It should not return anything.
+ */
+let arr = [5, 3, 8, 1];
 
-const red_p = document.createElement("p");
-red_p.setAttribute("style", "color: red");
-red_p.textContent = "Hey I'm red!";
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
 
-const blue_h3 = document.createElement("h3");
-blue_h3.setAttribute("style", "color: blue");
-blue_h3.textContent = "I'm blue h3!";
-
-const div_bp = document.createElement("div");
-div_bp.setAttribute("style", "border: solid black; background-color: pink;");
-div_bp.textContent = "div";
-
-container.append(red_p, blue_h3, div_bp);
-
-
-const h1 = document.createElement("h1");
-h1.textContent = "I'm in a div";
-
-const p = document.createElement("p");
-p.textContent = "ME TOO";
-
-div_bp.append(h1, p);
-
-
-let btn = document.querySelector('#btn');
-
-btn.addEventListener('click', function(event) {
-    console.log('The button was clicked!');
-    // event.stopPropagation();
-});
-
-document.body.addEventListener('click',function(event) {
-    console.log('The body was clicked!');
-});
-
-let isEven = (number) => number % 2 === 0;
-let	triple = number => number * 3;
-let	sum = (total, currentItem) => total + currentItem;
-
-function	sumOfTripledEvens(array)
+function	isFrom(item, begin, end)
 {
-	let	evens = array.filter(isEven);
-	let	tripledEvens = evens.map(triple);
-	let	sumTE = tripledEvens.reduce(sum, 0);
-	return (sumTE);
+	return (item >= begin && item <= end);
 }
 
-let	myArray = [10, 1, 3, 4, 5, 6];
-console.log(sumOfTripledEvens(myArray));
-console.log(sumOfTripledEvens([]));
+function	filterRangeInPlace(arr, begin, end)
+{
+	for (const item of arr)
+	{
+	// filter array
+		if (!isFrom(item, begin, end))
+// remove unwanted item
+			arr.splice(arr.indexOf(item), 1);
+	}
+
+}
+
+console.log( arr ); // [3, 1]
+
